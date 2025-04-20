f9bd3ad6a7f63f6bb0d031406798db35c76d2885
adrian zafra <zafrabeltran.adrian@gmail>
2025-04-20 16:41:49 +0200

    comit inicial : clase calculadora con el metodo suma

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+// Calculadora.java
+public class Calculadora {
+    private int num1;
+    private int num2;
+
+    public Calculadora(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int suma() {
+        return num1 + num2;
+    }
+}
